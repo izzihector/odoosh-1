5ab352c5c9412baf115331f31f2e882e8b38741f
captivea-jbb <56834381+captivea-jbb@users.noreply.github.com>
2020-08-10 13:49:28 -0400

    Adding the cap_product_page module
    Adding the module which handles the display of the qty in stock for each variant selection

---
+++ cap_product_page/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Captivea Product Page',
+    'category': 'Website',
+    'sequence': 5,
+    'summary': 'Captivea Product Page',
+    'version': '1.0',
+    'author': 'Captivea',
+    'support': '',
+    'website': '',
+    'license': 'OPL-1',
+    'description': """
+        """,
+    'depends': ['website_mass_mailing', 'website_blog', 'website_sale_wishlist', 'website_sale_comparison', 'website_sale_stock', 'website_crm', 'website_event', 'sale'],
+    'data': [
+        'views/assets.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
